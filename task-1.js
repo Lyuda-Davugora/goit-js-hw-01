506318ecbd1f9fb9c3bb71ed849afde52b09be7c
Lyudmila Davugora <161765335+Lyuda-Davugora@users.noreply.github.com>
2024-05-16 19:15:29 +0300

    Add files via upload

---
+++ task-1.js
@@ -0,0 +1,17 @@
+function makeTransaction(quantity, pricePerDroid) {
+<<<<<<< HEAD
+const totalPrice = quantity * pricePerDroid;
+return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+=======
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+>>>>>>> 43ba812f579a1d53184e2be7ab1cb5cd493cf0f0
+}
+
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+<<<<<<< HEAD
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+=======
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+>>>>>>> 43ba812f579a1d53184e2be7ab1cb5cd493cf0f0
